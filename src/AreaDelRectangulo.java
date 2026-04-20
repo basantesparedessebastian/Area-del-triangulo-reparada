ca5afd6a1eecad507c386a2aed00a5de95861cde
sebastian <basantesparedessebastian@gmail.com>
2026-04-19 22:41:06 -0500

    Agrego AreaDelRectangulo

---
+++ src/AreaDelRectangulo.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class AreaDelRectangulo {
+public void AreaDelRectangulo(){
+    Scanner sc =new Scanner(System.in);
+    Decimal longitud ,ancho,area
+    System.out.println("introduzaca una longiutd en metros");
+      double base =5.0
+
+    System.out.println("introduzaca una altura en metros");
+    double altura = 3.0
+    
+    double area = base * altura;
+    System.out.println("el area del rectangulo es: " + area);
+
+}
+}
+}
